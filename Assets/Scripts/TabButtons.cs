3a167b4f5d4ae9603c21ffee90c0dbf5d356d670
gruby1265 <mikolaj.gosztyla@szkola.salezjanie.com>
2020-05-26 19:50:55 +0200

    nie wiem co

---
+++ Assets/Scripts/TabButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(image))]
+public class TabButtons : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    int tabIndex;
+    Image image;
+    TabController controller;
+    bool isActive = false;
+
+    Awake(){
+        controller = FindObjectOfType<TabController>();
+        image = GetComponent<Image>();
+    }
+
+    public void SetIndex(int index){
+        tabIndex = index;
+    }
+
+    public void OnPointerClick(PointerEventData eventData){
+        controller.ButtonMouseClick(tabIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        if (!isActive){
+            image.color = controller.mouseEnterColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        if (!isActive){
+            image.color = controller.normalColor;
+        }
+    }
+
+    public void ToggleActive(){
+        isActive = !isActive;
+        if(isActive){
+            image.color = controller.mouseClickColor;
+        }else{
+            image.color = controller.normalColor;
+        }
+    }
+}
---
+++ Assets/Scripts/TabController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabController : MonoBehaviour
+{
+    public Transform buttonsPanel;
+    public Transform panelsPanel;
+
+    public Color normalColor;
+    public Color mouseClickColor;
+    public Color mouseEnterColor;
+
+    int selectedTab;
+    TabButtons selectedButton;
+    
+
+}
